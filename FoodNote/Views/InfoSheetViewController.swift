c24baa422bf880b9b30cad95f00aef12d83eedd2
Julian <xjulian1010@outlook.com>
2024-06-09 18:02:20 -0600

    [Update] Building out view for saving unit preferences - some more logic should complete

--- FoodNote/Views/InfoSheetViewController.swift
+++ FoodNote/Views/InfoSheetViewController.swift
@@ -74,6 +74,14 @@ class InfoSheetViewController: UIViewController {
         return button
     }()
     
+    private var goToUnitSelectionButton: UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(systemName: "globe")
+        button.setImage(image, for: .normal)
+        button.addTarget(self, action: #selector(presentUnitOptionsViewController), for: .touchUpInside)
+        return button
+    }()
+    
     private var errorLabel: UILabel = {
        let label = UILabel()
         label.text = "Enter current weight and a goal"
@@ -155,6 +163,12 @@ class InfoSheetViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @objc func presentUnitOptionsViewController () {
+        let vc = UnitOptionsViewController(user: user)
+        vc.modalPresentationStyle = .popover
+        present(vc, animated: true)
+    }
+    
     func setup () {
         view.addSubview(enterInfoTextLabel) //add to view first
         enterInfoTextLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
@@ -197,6 +211,11 @@ class InfoSheetViewController: UIViewController {
         doneButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         doneButton.widthAnchor.constraint(equalToConstant: 120).isActive = true
         doneButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        view.addSubview(goToUnitSelectionButton)
+        goToUnitSelectionButton.translatesAutoresizingMaskIntoConstraints = false
+        goToUnitSelectionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        goToUnitSelectionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
     }
     
     func getCalorieGoal () -> Int {
@@ -226,182 +245,3 @@ struct InfoSheet: UIViewControllerRepresentable {
 #Preview {
     InfoSheet()
 }
-
-///InfoSheetViewControllerDeprecated
-//class InfoSheetViewControllerDeprecated: UIViewController {
-//    let user = User()
-//    var seguedFromSettings: Bool = false
-//
-//    init(seguedFromSettings: Bool) {
-//        super.init(nibName: nil, bundle: nil)
-//        self.seguedFromSettings = seguedFromSettings
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//    private lazy var enterInfoTextLabel: UILabel = {
-//        let label = UILabel()
-//        label.text = !self.seguedFromSettings ? "Enter some info about you" : "Update weight"
-//        label.textColor = .white
-//        label.textAlignment = .center
-//        label.font = .systemFont(ofSize: 24, weight: .bold)
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        return label
-//    }()
-//
-//    private lazy var weightTextField: UITextField = {
-//        let textField = UITextField()
-//        textField.placeholder = "LBS"
-//        textField.textColor = UIColor.black
-//        textField.borderStyle = .roundedRect
-//        textField.backgroundColor = UIColor.white.withAlphaComponent(0.8)
-//        textField.translatesAutoresizingMaskIntoConstraints = false
-//        textField.keyboardType = .decimalPad
-//        textField.textAlignment = .center
-//        return textField
-//    }()
-//
-//    private lazy var goalWeightTextField: UITextField = {
-//        let textField = UITextField()
-//        textField.placeholder = "Goal Weight"
-//        textField.textColor = UIColor.black
-//        textField.borderStyle = .roundedRect
-//        textField.backgroundColor = UIColor.white.withAlphaComponent(0.8)
-//        textField.translatesAutoresizingMaskIntoConstraints = false
-//        textField.keyboardType = .decimalPad
-//        textField.textAlignment = .center
-//        return textField
-//    }()
-//
-//    private var xButton: UIButton = {
-//        let button = UIButton(type: .system)
-//        let xImage = UIImage(systemName: "xmark")?.withTintColor(.red, renderingMode: .alwaysOriginal)
-//        button.setImage(xImage, for: .normal)
-//        button.addTarget(self, action: #selector(xButtonClicked), for: .touchUpInside)
-//        return button
-//    }()
-//
-//    private var doneButton: UIButton = {
-//        let button = UIButton(type: .system)
-//        button.translatesAutoresizingMaskIntoConstraints = false
-//        button.setTitle("DONE", for: .normal)
-//        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .bold)
-//        button.backgroundColor = UIColor.white.withAlphaComponent(0.8)
-//        button.setTitleColor(.blue, for: .normal)
-//        button.layer.cornerRadius = 15
-//        button.layer.shadowColor = UIColor.black.cgColor
-//        button.layer.shadowOpacity = 0.3
-//        button.layer.shadowOffset = CGSize(width: 0, height: 5)
-//        button.layer.shadowRadius = 10
-//        button.addTarget(self, action: #selector(doneButtonClicked), for: .touchUpInside)
-//        return button
-//    }()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        setupGradientBackground()
-//        self.setup()
-//    }
-//
-//    @objc func doneButtonClicked() {
-//        // MARK: USER DEFAULTS START CODE
-//
-//        if self.seguedFromSettings && ((weightTextField.text?.isEmpty) != nil) && ((goalWeightTextField.text?.isEmpty) != nil) {
-//            self.dismiss(animated: true)
-//        }
-//
-//        guard let userWeightText = weightTextField.text else { return }
-//        guard let userWeightDouble = Double(userWeightText) else { return }
-//
-//        let manager = CoreDataManager.shared
-//        let weight = LoggedWeight(context: manager.persistentContainer.viewContext)
-//        weight.weight = userWeightDouble
-//        weight.dateLogged = Date()
-//        manager.save()
-//
-//        guard let userGoalText = goalWeightTextField.text else { return }
-//        guard let userGoalDouble = Double(userGoalText) else { return }
-//        self.user.saveUserGoalWeight(weight: userGoalDouble)
-//
-//        if !self.seguedFromSettings {
-//            self.user.saveUserLoadedApp()
-//        }
-//
-//        // MARK: USER DEFAULTS END CODE
-//
-//        if self.seguedFromSettings {
-//            self.dismiss(animated: true, completion: nil)
-//        } else if !self.seguedFromSettings {
-//            let vc = CalorieViewController(calorieAmount: self.getCalorieGoal(), user: self.user)
-//            vc.modalPresentationStyle = .fullScreen
-//            vc.modalTransitionStyle = .flipHorizontal
-//            present(vc, animated: true)
-//        }
-//    }
-//
-//    @objc func xButtonClicked() {
-//        self.dismiss(animated: true, completion: nil)
-//    }
-//
-//    func setup() {
-//        view.addSubview(enterInfoTextLabel) // Add to view first
-//
-//        enterInfoTextLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-//        enterInfoTextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//
-//        let stack = UIStackView(arrangedSubviews: [
-//            weightTextField,
-//            goalWeightTextField,
-//        ])
-//        stack.spacing = 20
-//        stack.axis = .vertical
-//        stack.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(stack)
-//        stack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        stack.topAnchor.constraint(equalTo: enterInfoTextLabel.bottomAnchor, constant: 40).isActive = true
-//
-//        weightTextField.widthAnchor.constraint(equalToConstant: 200).isActive = true
-//        weightTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
-//
-//        goalWeightTextField.widthAnchor.constraint(equalToConstant: 200).isActive = true
-//        goalWeightTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
-//
-//        view.addSubview(doneButton)
-//        doneButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30).isActive = true
-//        doneButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        doneButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
-//        doneButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-//
-//        if self.seguedFromSettings {
-//            view.addSubview(xButton)
-//            xButton.translatesAutoresizingMaskIntoConstraints = false
-//            xButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
-//            xButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-//        }
-//    }
-//
-//    func setupGradientBackground() {
-//        let gradientLayer = CAGradientLayer()
-//        let safeAreaInsets = view.safeAreaInsets
-//        let frame = CGRect(
-//            x: view.bounds.origin.x,
-//            y: view.bounds.origin.y - safeAreaInsets.top,
-//            width: view.bounds.width,
-//            height: view.bounds.height + safeAreaInsets.top + safeAreaInsets.bottom
-//        )
-//        gradientLayer.frame = frame
-//        gradientLayer.colors = [UIColor.systemBlue.cgColor, UIColor.systemPurple.cgColor]
-//        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
-//        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
-//        view.layer.insertSublayer(gradientLayer, at: 0)
-//    }
-//
-//
-//    func getCalorieGoal() -> Int {
-//        guard let weightInt = Int(weightTextField.text ?? "1") else { return 0 }
-//        guard let goalWghtInt = Int(goalWeightTextField.text ?? "1") else { return 0 }
-//        return weightInt > goalWghtInt ? 1700 : 2300
-//    }
-//}
---
+++ FoodNote/Views/Subviews/UnitOptionsViewController.swift
@@ -0,0 +1,255 @@
+//
+//  UnitOptionsViewController.swift
+//  FoodNote
+//
+//  Created by Julian 沙 on 6/9/24.
+//
+
+import UIKit
+import SwiftUI
+
+enum SelectedWeightOption {
+    case lbs
+    case kg
+    case stone
+}
+
+enum SelectedEnergyUnit {
+    case cal
+    case kcal
+}
+
+class UnitOptionsViewController: UIViewController {
+    
+    var user: User
+    var selectedOption: SelectedWeightOption = .lbs
+    var selectedEnergyUnit: SelectedEnergyUnit = .cal
+    
+    private var unitPreferenceLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Unit Preference"
+        label.textColor = .black
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 26)
+        return label
+    }()
+    
+    private var lbsButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("LBS", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 15
+        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        button.addTarget(self, action: #selector(selectLbs), for: .touchUpInside)
+        return button
+    }()
+    
+    private var kgButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Kilogram", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 15
+        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        button.addTarget(self, action: #selector(selectKg), for: .touchUpInside)
+        return button
+    }()
+    
+    private var stoneButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Stone", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 15
+        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        button.addTarget(self, action: #selector(selectStone), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var dividerLine: UIView = {
+        let dividerView: UIView = UIView()
+        dividerView.backgroundColor = .gray
+        dividerView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        dividerView.heightAnchor.constraint(equalToConstant: 5).isActive = true
+        return dividerView
+    }()
+    
+    private var calButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Calories", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 15
+        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        button.addTarget(self, action: #selector(selectCal), for: .touchUpInside)
+        return button
+    }()
+    
+    private var kcalButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("KCal", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 15
+        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        button.addTarget(self, action: #selector(selectKCal), for: .touchUpInside)
+        return button
+    }()
+    
+    private var saveButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("SAVE", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 15
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        button.addTarget(self, action: #selector(saveButtonPressed), for: .touchUpInside)
+        button.backgroundColor = .systemBlue
+        return button
+    }()
+    
+    @objc func selectLbs () {
+        self.selectedOption = .lbs
+        self.lbsButton.backgroundColor = .systemBlue
+        self.kgButton.backgroundColor = .lightGray
+        self.stoneButton.backgroundColor = .lightGray
+    }
+    
+    @objc func selectKg () {
+        self.selectedOption = .kg
+        self.kgButton.backgroundColor = .systemBlue
+        self.lbsButton.backgroundColor = .lightGray
+        self.stoneButton.backgroundColor = .lightGray
+    }
+    
+    @objc func selectStone () {
+        self.selectedOption = .stone
+        self.stoneButton.backgroundColor = .systemBlue
+        self.lbsButton.backgroundColor = .lightGray
+        self.kgButton.backgroundColor = .lightGray
+    }
+    
+    @objc func selectCal () {
+        self.selectedEnergyUnit = .cal
+        self.calButton.backgroundColor = .systemBlue
+        self.kcalButton.backgroundColor = .lightGray
+    }
+    
+    @objc func selectKCal () {
+        self.selectedEnergyUnit = .kcal
+        self.kcalButton.backgroundColor = .systemBlue
+        self.calButton.backgroundColor = .lightGray
+    }
+    
+    @objc func saveButtonPressed () {
+        switch self.selectedOption {
+        case .lbs:
+            user.saveUserUnitPreference(preference: .lbs)
+        case .kg:
+            user.saveUserUnitPreference(preference: .kg)
+        case .stone:
+            user.saveUserUnitPreference(preference: .stone)
+        }
+        switch self.selectedEnergyUnit {
+        case .cal:
+            user.saveUserEnergyPreference(preference: .cal)
+        case .kcal:
+            user.saveUserEnergyPreference(preference: .kcal)
+        }
+        self.dismiss(animated: true, completion: {
+            //some completion here
+        })
+    }
+    
+    init(user: User) {
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.overrideUserInterfaceStyle = .light
+        arrange()
+        self.selectedOption = switch user.getUnitPreference() {
+        case .lbs:
+                .lbs
+        case .kg:
+                .kg
+        case .stone:
+                .stone
+        }
+        self.selectedEnergyUnit = switch user.getUserEnergyPreference() {
+        case .cal:
+                .cal
+        case .kcal:
+                .kcal
+        }
+    }
+    
+    func arrange () {
+        
+        view.addSubview(unitPreferenceLabel)
+        unitPreferenceLabel.translatesAutoresizingMaskIntoConstraints = false
+        unitPreferenceLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        unitPreferenceLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 16).isActive = true
+        
+        let weightButtonStack: UIStackView = UIStackView(arrangedSubviews: [
+            lbsButton,
+            kgButton,
+            stoneButton
+        ])
+        
+        self.lbsButton.backgroundColor = selectedOption == .lbs ? .systemBlue : .clear
+        self.kgButton.backgroundColor = selectedOption == .kg ? .systemBlue : .lightGray
+        self.stoneButton.backgroundColor = selectedOption == .stone ? .systemBlue : .lightGray
+
+        weightButtonStack.translatesAutoresizingMaskIntoConstraints = false
+        weightButtonStack.axis = .vertical
+        weightButtonStack.spacing = 40
+        view.addSubview(weightButtonStack)
+        weightButtonStack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        weightButtonStack.topAnchor.constraint(equalTo: unitPreferenceLabel.safeAreaLayoutGuide.topAnchor, constant: 90).isActive = true
+        view.addSubview(dividerLine)
+        dividerLine.translatesAutoresizingMaskIntoConstraints = false
+        dividerLine.centerXAnchor.constraint(equalTo: weightButtonStack.centerXAnchor).isActive = true
+        dividerLine.topAnchor.constraint(equalTo: weightButtonStack.bottomAnchor, constant: 16).isActive = true
+        
+        let energyUnitStack: UIStackView = UIStackView(arrangedSubviews: [
+        calButton,
+        kcalButton
+        ])
+        
+        self.calButton.backgroundColor = selectedEnergyUnit == .cal ? .systemBlue : .clear
+        self.kcalButton.backgroundColor = selectedEnergyUnit == .kcal ? .systemBlue : .lightGray
+        
+        energyUnitStack.translatesAutoresizingMaskIntoConstraints = false
+        energyUnitStack.axis = .vertical
+        energyUnitStack.spacing = 40
+        view.addSubview(energyUnitStack)
+        energyUnitStack.topAnchor.constraint(equalTo: dividerLine.bottomAnchor, constant: 40).isActive = true
+        energyUnitStack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        view.addSubview(saveButton)
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        saveButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -26).isActive = true
+        saveButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+}
+
+struct UnitViewControllerRepresentative: UIViewControllerRepresentable {
+    var seguedFromSettings: Bool = false
+    func makeUIViewController(context: Context) -> some UIViewController {
+        return UnitOptionsViewController(user: User())
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
+
+
+#Preview {
+    UnitViewControllerRepresentative()
+}
